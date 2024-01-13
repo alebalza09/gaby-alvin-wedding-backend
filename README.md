a59a3a96bc4f784d1cd3ba60f26c70d1d257ccaa
alebalza09 <alexandrabalza09@gmail.com>
2024-01-13 13:59:23 +0100

    create post rsvp

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# gaby-alvin-wedding-backend
+# Gaby&Alvaro Wedding 2024
+
+This project was created using NodeJs + Express + PostgreSQL
+
+### Run project locally
+
+1. Create your postgresql db and add to the .env file the params to access your local database and the port for running the server
+
+- PORT
+- DB_USER
+- DB_PASSWORD
+- DB_NAME
+- DB_HOST
+- MAX_POOL_SIZE
+- LOGGER_LEVEL
+- DB_PORT
+
+2. Install all the dependencies
+```bash
+npm install
+```
+3. Run migrations
+```bash
+npm run migrate
+```
+4. Run seed
+```bash
+npm run seed
+```
+5. Run the server in specified
+```bash
+npm run dev
+```
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,55 @@
-import express from 'express';
+import express, { Application } from 'express';
+import dotenv from 'dotenv';
+import * as path from 'path';
+import bodyParser from 'body-parser';
+import { Pool } from 'pg';
 
-const app = express();
-const port = 3000;
+const app: Application = express();
+dotenv.config({ path: path.join(process.cwd(), 'dist', '.env') });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const env = {
+  port: process.env.PORT
+}
+
+app.use(bodyParser.json());
+
+// PostgreSQL connection pool
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: 5432,
 });
+
+interface IGuest {
+  firstName: string;
+  lastName: string;
+  email: string;
+  rsvp: boolean;
+  plusOne: boolean;
+  totalCompanions: number;
+}
+
+app.post('/api/rsvp', async (req, res) => {
+  try {
+      const { firstName, lastName, email, rsvp, plusOne, totalCompanions }: IGuest = req.body;
+      const result = await pool.query(
+          'INSERT INTO guests (firstName, lastName, email, rsvp, plusOne, totalCompanions) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+          [firstName, lastName, email, rsvp, plusOne, totalCompanions]
+      );
+
+      res.json(result.rows[0]);
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+if (env.port) {
+  app.listen(env.port, () => {
+    console.log(`Server is running on port ${env.port}`);
+  });
+} else {
+  console.error(`Missing env variables`);
+}
